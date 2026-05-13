dac01917e674955b03512f7642a596430bd4917c
jfosset <jfosset@etu.uqac.ca>
2026-05-13 18:51:48 -0400

    add serialization of u8, Quat, Vec3 and Transform

---
+++ network_serialization/src/base_type.rs
@@ -0,0 +1,70 @@
+﻿use bytes::{Buf, BufMut, Bytes};
+use ordered_float::NotNan;
+use crate::{Deserializable, Serializable, SerializationError};
+
+impl Serializable for NotNan<f32>
+{
+    fn serialize(self, stream: &mut bytes::BytesMut) -> Result<(), SerializationError>
+    {
+        let bytes = serialize_f32(self);
+        stream.put_slice(&bytes);
+
+        Ok(())
+    }
+}
+
+impl Deserializable for NotNan<f32>
+{
+    fn deserialize(bytes: &mut Bytes) -> Self
+    {
+        deserialize_f32(bytes)
+    }
+}
+
+impl Serializable for f32
+{
+    fn serialize(self, stream: &mut bytes::BytesMut) -> Result<(), SerializationError>
+    {
+        let not_nan_self = NotNan::new(self)?;
+        not_nan_self.serialize(stream)
+    }
+}
+
+impl Deserializable for f32
+{
+    fn deserialize(bytes: &mut Bytes) -> Self
+    {
+        let value = NotNan::deserialize(bytes);
+        value.into_inner()
+    }
+}
+
+impl Serializable for u8
+{
+    fn serialize(self, stream: &mut bytes::BytesMut) -> Result<(), SerializationError>
+    {
+        stream.put_u8(self);
+        Ok(())
+    }
+}
+
+impl Deserializable for u8
+{
+    fn deserialize(bytes: &mut Bytes) -> Self
+    {
+        bytes.get_u8()
+    }
+}
+
+fn serialize_f32(value: NotNan<f32>) -> [u8; 2]
+{
+    let value = value * 100.0;
+    let value = value.trunc() as i16;
+    value.to_be_bytes()
+}
+
+fn deserialize_f32(bytes: &mut bytes::Bytes) -> NotNan<f32>
+{
+    let value = bytes.get_i16();
+    unsafe { NotNan::new_unchecked(value as f32 / 100.0) }
+}
---
+++ network_serialization/src/lib.rs
@@ -0,0 +1,34 @@
+﻿use bytes::Bytes;
+use ordered_float::FloatIsNan;
+
+pub mod bevy;
+pub mod base_type;
+
+pub trait Packet
+{
+    fn read(bytes: Bytes) -> Self;
+    fn write(self) -> bytes::Bytes;
+}
+
+pub trait Serializable
+{
+    fn serialize(self, stream: &mut bytes::BytesMut) -> Result<(), SerializationError>;
+}
+
+pub trait Deserializable
+{
+    fn deserialize(bytes: &mut Bytes) -> Self;
+}
+
+pub enum SerializationError
+{
+    BadArguments,
+}
+
+impl From<FloatIsNan> for SerializationError
+{
+    fn from(_: FloatIsNan) -> SerializationError
+    {
+        SerializationError::BadArguments
+    }
+}
