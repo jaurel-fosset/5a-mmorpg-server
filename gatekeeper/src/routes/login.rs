99f5ab374d53b975a383e82b98ca66d048cbc126
CL3M3N7 <146713907+CL3M3N7@users.noreply.github.com>
2026-05-18 19:56:52 -0400

    feat(gatekeeper): handle /login & return available server

--- gatekeeper/src/routes/login.rs
+++ gatekeeper/src/routes/login.rs
@@ -1,6 +1,10 @@
 use crate::AppState;
 use axum::extract::State;
-use axum::{Json, http::StatusCode, response::{IntoResponse, Response}};
+use axum::{
+    Json,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::sync::Arc;
@@ -14,6 +18,14 @@ use tokio::sync::Mutex;
 //     -d '{"username":"foo"}' \
 //     http://localhost:3000/login
 
+// Commande à réaliser sur Redis pour réaliser le premier test en attendant l'orchestrator
+// > HSET gameserver:gs-01 ip "127.0.0.1" port "7001" cpu "51" ram "20" players "64" max_players "120" zone "America/Toronto"
+// > HSET gameserver:gs-02 ip "127.0.0.1" port "7002" cpu "80" ram "70" players "110" max_players "120" zone "Europe/Paris"
+// > HSET gameserver:gs-03 ip "127.0.0.1" port "7003" cpu "1" ram "12" players "3" max_players "100" zone "America/New_York"
+// > ZADD servers:available 10 "gameserver:gs-01"
+// > ZADD servers:available 80 "gameserver:gs-02"
+// > ZADD servers:available 50 "gameserver:gs-03"
+
 pub async fn login(
     State(state): State<Arc<Mutex<AppState>>>,
     Json(payload): Json<AuthPayload>,
@@ -23,15 +35,41 @@ pub async fn login(
     }
 
     let mut state = state.lock().await;
-    redis::cmd("SET")
-        .arg(&["key", "foo"])
-        .exec_async(&mut state.redis_connexion)
+
+    let game_server: Vec<String> = redis::cmd("ZRANGE")
+        .arg(&["servers:available", "0", "0"])
+        .query_async(&mut state.redis_connexion)
         .await
         .unwrap();
 
-    let token = "127.0.0.1:7777".to_owned();
+    if game_server.is_empty() {
+        return Err(AuthError::NoServerAvailable);
+    }
 
-    Ok(Json(AuthBody::new(token)))
+    let info_server: Vec<String> = redis::cmd("HGETALL")
+        .arg::<Vec<String>>(game_server)
+        .query_async(&mut state.redis_connexion)
+        .await
+        .unwrap();
+
+    let mut ip: String = "".to_owned();
+    let mut port: u16 = 0;
+    let mut zone: String = "".to_owned();
+
+    let number_data = info_server.len() / 2;
+    for n in 0..number_data {
+        if info_server[n * 2] == "ip" {
+            ip = info_server[n * 2 + 1].clone();
+        }
+        if info_server[n * 2] == "port" {
+            port = info_server[n * 2 + 1].parse::<u16>().unwrap();
+        }
+        if info_server[n * 2] == "zone" {
+            zone = info_server[n * 2 + 1].clone();
+        }
+    }
+
+    Ok(Json(AuthBody::new(ip, port, zone)))
 }
 
 #[derive(Debug, Deserialize)]
@@ -40,24 +78,30 @@ pub struct AuthPayload {
 }
 #[derive(Debug, Serialize)]
 pub struct AuthBody {
-    server_ip: String,
+    ip: String,
+    port: u16,
+    zone: String,
 }
 
 impl AuthBody {
-    fn new(server_ip: String) -> Self {
-        Self { server_ip }
+    fn new(ip: String, port: u16, zone: String) -> Self {
+        Self { ip, port, zone }
     }
 }
 
 #[derive(Debug)]
 pub enum AuthError {
     MissingCredentials,
+    NoServerAvailable,
 }
 
 impl IntoResponse for AuthError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
             AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::NoServerAvailable => {
+                (StatusCode::SERVICE_UNAVAILABLE, "No server available")
+            }
         };
         let body = Json(json!({
             "error": error_message,
