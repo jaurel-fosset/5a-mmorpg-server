6de56115ff1c13b97b0c8473921d94dcb03a8add
jfosset <jfosset@etu.uqac.ca>
2026-05-20 19:12:42 -0400

    add plugins to game_server main

--- game_server/src/main.rs
+++ game_server/src/main.rs
@@ -1,6 +1,12 @@
+use bevy::diagnostic::{DiagnosticsPlugin, SystemInformationDiagnosticsPlugin};
 use bevy::prelude::*;
+use game_server::env_parameter::EnvParameterPlugin;
 
 fn main()
 {
-    App::new().run();
+    App::new()
+        .add_plugins(DiagnosticsPlugin)
+        .add_plugins(SystemInformationDiagnosticsPlugin)
+        .add_plugins(EnvParameterPlugin)
+        .run();
 }
