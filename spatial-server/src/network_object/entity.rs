07788a4e138ecfa0faa1d96fb674de92e4f449ef
jaurel-fosset <jfosset@etu.uqac.ca>
2026-06-01 17:32:25 -0400

    (feat) add network object and their manager to spatial server

---
+++ spatial-server/src/network_object/entity.rs
@@ -0,0 +1,65 @@
+﻿use std::collections::HashSet;
+use crate::geometry::prelude as geo;
+use crate::network_object::shard::ShardId;
+
+pub struct EntityManager
+{
+    entities: Vec<Entity>,
+}
+
+impl EntityManager
+{
+    pub fn new() -> Self
+    {
+        EntityManager { entities: Vec::new() }
+    }
+    
+    pub fn receive_new_entities(&mut self, entities: &[(EntityId, geo::Position, ShardId)])
+    {
+        let entities = entities.into_iter()
+            .map(|(entity_id, pos, shard_id)| {
+                Entity::new(*entity_id, *pos, *shard_id)
+            });
+        self.entities.extend(entities);
+    }
+}
+
+#[derive(Debug)]
+pub struct Entity
+{
+    id: EntityId,
+    position: geo::Position,
+    current_shard: ShardId,
+    subscribed_shard: HashSet<ShardId>,
+}
+
+impl Entity
+{
+    fn new(id: EntityId, position: geo::Position, current_shard: ShardId) -> Self
+    {
+        Self
+        {
+            id,
+            position,
+            current_shard,
+            subscribed_shard: HashSet::new(),
+        }
+    }
+    
+    pub fn update_subscription(&mut self, shards: HashSet<ShardId>) -> Vec<ShardId>
+    {
+        let removed = self.subscribed_shard.difference(&shards)
+            .copied().collect::<Vec<_>>();
+        self.subscribed_shard = shards;
+        removed
+    }
+    
+    pub fn switch_current_shard(&mut self, new_shard: ShardId)
+    {
+        self.current_shard = new_shard;
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct EntityId(u64);
+
---
+++ spatial-server/src/network_object/shard.rs
@@ -0,0 +1,130 @@
+﻿use std::collections::HashMap;
+use std::time;
+use crate::geometry::prelude as geo;
+
+pub struct ShardManager
+{
+    shards: HashMap<ShardId, Shard>,
+    deleted_in_use: HashMap<ShardId, (time::Instant, Shard)>,
+    replaced_shards: HashMap<ShardId, ShardId>,
+    free_shards: HashMap<ShardId, time::Instant>,
+}
+
+impl ShardManager
+{
+    pub fn new() -> Self
+    {
+        ShardManager
+        {
+            shards: HashMap::new(),
+            deleted_in_use: HashMap::new(),
+            replaced_shards: HashMap::new(),
+            free_shards: HashMap::new(),
+        }
+    }
+
+    pub fn new_shard(&mut self, authority_bounds: geo::Rect, subscribe_bounds: geo::Rect) -> Option<ShardId>
+    {
+        let shard_id = self.get_free_shard()?;
+
+        self.free_shards.remove(&shard_id);
+        self.shards.insert(shard_id, Shard::new(authority_bounds, subscribe_bounds));
+
+        Some(shard_id)
+    }
+
+    // TODO : implement a function to clean up replaced shard
+
+    pub fn on_receive_shard_creation(&mut self, shard_created: ShardId)
+    {
+        match self.get_deleted_in_use_shard()
+        {
+            None => {}
+            Some(deleted_shard_id) =>
+            {
+                let (_, deleted_shard) = &self.deleted_in_use[&deleted_shard_id];
+
+                self.shards.insert(shard_created, Shard::new(deleted_shard.authority_bound, deleted_shard.subscribe_bound));
+                self.replaced_shards.insert(deleted_shard_id, shard_created);
+                self.deleted_in_use.remove(&deleted_shard_id);
+            }
+        }
+        self.free_shards.insert(shard_created, time::Instant::now());
+    }
+
+    pub fn on_receive_shard_deletion(&mut self, deleted_shard: ShardId)
+    {
+        match self.free_shards.remove(&deleted_shard)
+        {
+            Some(_) => return,
+            None => (),
+        }
+
+        if     self.deleted_in_use.contains_key(&deleted_shard)
+            || self.replaced_shards.contains_key(&deleted_shard)
+        {
+            eprintln!("Error: Double deletion on network, ignoring");
+            return;
+        }
+
+        let shard = match self.shards.remove(&deleted_shard)
+        {
+            Some(shard) => shard,
+            None =>
+            {
+                eprintln!("Error : deleted shard was not in any of our cache");
+                return;
+            },
+        };
+
+        eprintln!("Catastrophic error : shard in use was deleted");
+        match self.new_shard(shard.authority_bound, shard.subscribe_bound)
+        {
+            Some(new_shard) =>
+            {
+                println!("We were able to recover using another shard");
+                self.replaced_shards.insert(deleted_shard, new_shard);
+            }
+            None =>
+            {
+                println!("Requesting another shard created");
+                self.deleted_in_use.insert(deleted_shard, (time::Instant::now(), shard));
+                // TODO : request new shard on the network
+                return;
+            }
+        };
+    }
+
+    fn get_free_shard(&self) -> Option<ShardId>
+    {
+        self.shards.keys().copied().next()
+    }
+
+    fn get_deleted_in_use_shard(&self) -> Option<ShardId>
+    {
+        self.deleted_in_use.keys().copied().next()
+    }
+}
+
+pub struct Shard
+{
+    entities_count: usize,
+    authority_bound: geo::Rect,
+    subscribe_bound: geo::Rect,
+}
+
+impl Shard
+{
+    fn new(authority_bound: geo::Rect, subscribe_bound: geo::Rect) -> Self
+    {
+        Self
+        {
+            entities_count: 0,
+            authority_bound,
+            subscribe_bound,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct ShardId(u64);
